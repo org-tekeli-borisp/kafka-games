a1004624e5b9f26c235f6d04871525a3701a14d2
borisp <boris.pigin@gmail.com>
2024-08-14 18:27:31 +0200

    refactoring

--- src/test/kotlin/org/tekeli/borisp/kafkagames/ReadWrittenTest.kt
+++ src/test/kotlin/org/tekeli/borisp/kafkagames/ReadWrittenTest.kt
@@ -6,6 +6,7 @@ import org.apache.kafka.clients.producer.ProducerConfig
 import org.apache.kafka.common.serialization.StringDeserializer
 import org.apache.kafka.common.serialization.StringSerializer
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
@@ -25,22 +26,32 @@ class ReadWrittenTest {
 
     @Value("\${spring.kafka.topics.greeting}")
     lateinit var topic: String
+
+    lateinit var producer: KafkaTemplate<String, String>
+    lateinit var consumer: Consumer<String, String>
+
     @BeforeEach
     fun setup() {
         kafkaContainer.start()
+        producer = producer()
+        consumer = consumer()
+        consumer.subscribe(listOf(topic))
+    }
+
+    @AfterEach
+    fun tearDown() {
+        consumer.close(Duration.ofSeconds(10))
     }
 
     @Test
     fun shouldReadAWrittenMessage() {
-        val producer = producer()
-        val consumer = consumer()
-        consumer.subscribe(listOf(topic))
+        val message = "Hello, World!"
 
-        producer.send(topic, "Hello, World!")
+        producer.send(topic, message)
 
         val records = consumer.poll(Duration.ofSeconds(10))
         assertThat(records).isNotEmpty
-        val firstOrNull = records.firstOrNull { it.value() == "Hello, World!" }
+        val firstOrNull = records.firstOrNull { it.value() == message }
         assertThat(firstOrNull).isNotNull
     }
 
